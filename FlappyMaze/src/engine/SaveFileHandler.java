66f6c8c8f4a8d363e5d01414dcbd2de2a3efc4fa
StormRater <nicholas.johnson23@gmail.com>
2014-05-13 13:34:36 -0500

    Added Save handler bones

---
+++ FlappyMaze/src/engine/SaveFileHandler.java
@@ -0,0 +1,31 @@
+package engine;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
+public class SaveFileHandler {
+
+	Properties saveFile;
+	
+	
+	private SaveFileHandler() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void loadGameData(String fileName) {
+		
+	}
+	
+	public static void saveGameData() {
+		
+	}
+	
+	public static void saveGameData(String fileName) {
+		
+	}
+	
+	
+
+}
